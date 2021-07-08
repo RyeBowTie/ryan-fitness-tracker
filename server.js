8636bde2cc04609157caf57cecd8e1ac3a9052ae
Ryan O'Donohue <rod.odonohue@gmail.com>
2021-07-08 09:59:53 -0400

    updated mongodb connection.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const mongoose = require('mongoose');
 const express = require('express');
-// const models = require('./models');
+
 const routes = require('./controllers');
 const app = express();
 
@@ -13,10 +13,20 @@ app.use(express.static('public'));
 app.use(routes);
 
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/workoutdb", 
-    { useNewUrlParser: true });
+    process.env.MONGODB_URI || "mongodb://localhost/workout", 
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false    
+    });
+
+const db = mongoose.connection;
+
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected To Database'));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+    console.log(`App running at http://localhost:${PORT}/`);
 });
       
